c6967ff7f6b6e4645ed9246fadd44ee97789623e
Muhammad <muhammad.installer@gmail.com>
2023-01-04 23:46:19 +0600

    add 100.2 Interview _ Unique prime factors of a number.csproj

---
+++ src/Competitive Programming/100.2 Interview _ Unique prime factors of a number/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+
+namespace Unique_prime_factors_of_a_number;
+public class FactorCalculator
+{
+    public static bool isPrime(int x)
+    {
+        for (int i = 2; i <= Math.Sqrt(x); i++)
+        {
+            if (x % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static List<int> FindFactors(int number)
+    {
+
+        var list = new List<int>();
+
+
+        for (int i = 2; i <= number; i++)
+        {
+            if (isPrime(i) == true && number % i == 0)
+            {
+                list.Add(i);
+            }
+        }
+
+        return list;
+    }
+
+
+
+
+    public static void Main(string[] args)
+    {
+        var number = int.Parse(Console.ReadLine()!);
+
+        var list = FindFactors(number);
+
+        Console.WriteLine(string.Join("  ", list));
+    }
+
+}
